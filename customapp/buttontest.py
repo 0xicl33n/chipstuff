00bbebd49da6302c86b671ea935568e3970db442
Bonnie <0xicl33n@gmail.com>
2016-06-19 13:53:21 -0700

    Testing a "cleaner" script

--- customapp/buttontest.py
+++ customapp/buttontest.py
@@ -19,25 +19,18 @@ def keep_flat(event):
 
 ntcFont = tkFont.Font(family='Lato-Regular',size=14) # this is the font chip uses for its interface
 master.attributes('-fullscreen', True)
-master.configure(background='#4D4D4D')
+master.configure(background=WINDOW_BG)
 
-def sshd():
-	subprocess.Popen('sudo systemctl stop ssh',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+class createButton():
+	def __init__(self):
+		self.data = []
+		self.opts = 'height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0'
+		self.text = ''
+		self.cmd = ''
+		self.clicky = Button(master,self.text,self.cmd,self.opts)
 
-def sshe():
-	subprocess.Popen('sudo systemctl start ssh',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-def smbd():
-	subprocess.Popen('sudo systemctl stop samba',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-def smbe():
-	subprocess.Popen('sudo systemctl start samba',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-def vnce():
-	subprocess.Popen('sudo vncserver',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-
-def vncd():
-	subprocess.Popen('sudo vncserver -kill :1',stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+def runAcmd(cmd):
+	subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 
 
 def quitApp():
@@ -45,12 +38,13 @@ def quitApp():
 
 def pressed(self, index):
 	Button.buttons[index].configure(bg="red")
-ssh_e = Button(master, text="Start SSH", font=ntcFont, command=sshd, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
-ssh_d = Button(master, text="Stop SSH",font=ntcFont, command=sshe, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
-vnc_e = Button(master, text="Start VNC", font=ntcFont, command=vnce, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
-vnc_d = Button(master, text="Stop VNC", font=ntcFont, command=vncd, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
-#smb_e = Button(master, text="Start Samba", font=ntcFont, command=smbe, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
-#smb_d = Button(master, text="Stop Samba", font=ntcFont, command=smbd, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
+
+
+
+ssh_e = Button(master, text="Start SSH", font=ntcFont, command=runAcmd('sudo systemctl start ssh'), height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
+ssh_d = Button(master, text="Stop SSH",font=ntcFont, command=runAcmd('sudo systemctl stop ssh'), height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
+vnc_e = Button(master, text="Start VNC", font=ntcFont, command=runAcmd('sudo vncserver'), height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
+vnc_d = Button(master, text="Stop VNC", font=ntcFont, command=runAcmd('sudo vncserver -kill :1'), height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
 quit = Button(master, text="Quit", font=ntcFont, command=quitApp, height = B_HEIGHT, width = B_WIDTH, fg=BCHIP_FG, bg=BCHIP_BG, relief=FLAT,highlightthickness=0,bd=0)
 
 
